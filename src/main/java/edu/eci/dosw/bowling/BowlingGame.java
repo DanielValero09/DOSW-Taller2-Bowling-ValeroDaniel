28f65925ff4be16cf123983548bad9977b7d9029
DanielValero09 <daniel.valero-q@mail.escuelaing.edu.co>
2026-09-16 18:09:52 -0500

    feat: GREEN - valida limite maximo de pines

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -23,7 +23,7 @@ public class BowlingGame {
      * Lanza IllegalStateException si el juego ya termino.
      */
     public void roll(int pins) {
-        if (pins < 0) {
+        if (pins < 0 || pins > 10) {
             throw new IllegalArgumentException();
         }
         if (frames.isEmpty()) {
