a1a79fe7641751b212aa5414e68c5cb103b0c56e
DanielValero09 <daniel.valero-q@mail.escuelaing.edu.co>
2026-09-17 13:53:33 -0500

    feat: GREEN - implementa bono de spare

--- src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -6,9 +6,13 @@ public class BowlingScorer {
 
     public int calculate(List<Frame> frames) {
         int score = 0;
-        for (Frame frame : frames) {
+        for (int i = 0; i < frames.size(); i++) {
+            Frame frame = frames.get(i);
             for (int pins : frame.getRolls()) {
                 score += pins;
+            }
+            if (frame.getType() == FrameType.SPARE) {
+                score += frames.get(i + 1).getRolls().get(0);
             }
         }
         return score;
