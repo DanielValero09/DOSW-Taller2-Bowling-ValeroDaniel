b56b0be02b27b19cb6d52e215ce407588a13ab04
DanielValero09 <daniel.valero-q@mail.escuelaing.edu.co>
2026-09-17 13:07:08 -0500

    test: RED - juego de ceros obtiene puntaje cero

---
+++ src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -0,0 +1,22 @@
+package src.test.java.edu.eci.dosw.bowling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import src.main.java.edu.eci.dosw.bowling.BowlingGame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BowlingScorerTest {
+
+    @Test
+    @DisplayName("B1: a gutter game should score zero")
+    void gutterGameShouldScoreZero() {
+        BowlingGame game = new BowlingGame();
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        assertEquals(0, game.score());
+    }
+}
